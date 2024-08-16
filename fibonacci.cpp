7803b3664ccb2c3b3dba74b2bfd1bbe16029d9e6
Gustavo Marques Martins <161479140+GustavoMarques06@users.noreply.github.com>
2024-08-16 20:33:08 -0300

    fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int n1 = 1, n2 = 1;
+
+	printf("%d\n", n1);
+	printf("%d\n", n2);
+
+	for (int i = 0; i < 10; i++){
+		int aux = n1 + n2;
+		printf("%d\n", aux);
+		n1 = n2;
+		n2 = aux;
+	}
+}
